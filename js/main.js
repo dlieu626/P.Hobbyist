961bfd0729823cd3726358e10afd7a4e1f1b0f8f
Weihan-Jiang <wej014@ucsd.edu>
2016-10-26 00:33:58 -0700

    added materialize and swiper. created swipable page.

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(function() {
+    var mySwiper = new Swiper('.swiper-container', {
+        // Optional parameters
+        direction: 'horizontal',
+        loop: true,
+        pagination: '.swiper-pagination'
+
+    })
+});

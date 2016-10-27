a3535bb014ccaa7e83e9d17bea774656ec64ef6f
Weihan-Jiang <wej014@ucsd.edu>
2016-10-27 13:29:20 -0700

    very basic UI, nested swapper implemented, bugs for overlapping slider

--- js/main.js
+++ js/main.js
@@ -2,8 +2,22 @@ $(function() {
     var mySwiper = new Swiper('.swiper-container', {
         // Optional parameters
         direction: 'horizontal',
-        loop: true,
+        loop: false,
         pagination: '.swiper-pagination'
 
+    })
+    var nestedSwiper = new Swiper('.nestedswiper-container', {
+        direction: 'horizontal',
+        loop: false,
+        nested: true,
+        slidesPerView: 5,
+        spaceBetween: 30,
+        free: true,
+        breakpoints: {
+            767: {
+                slidesPerView: 3,
+                spaceBetween: 30
+            }
+        }
     })
 });
